71f4e46b7c51a12dfc560d94bfabf19ab8478d31
Grajj <steveson.khatri@gmail.com>
2016-07-17 14:29:33 +0545

    Add files via upload

---
+++ Q.3.cpp
@@ -0,0 +1,99 @@
+/* Program to convert infix expression to post fix expression
+* Author: Giriraj Khatri
+* Roll No: 0509
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#define max 100
+
+typedef struct{
+	int top;
+	char items[max];
+}stack;
+
+char push(stack *,char);
+int pop(stack *);
+int symbolValue(char);
+
+int main()
+{
+	int i,w,k,b=0;
+	char infix[max],a[max],y;
+	stack s;
+	s.top=-1;
+	
+	printf("enter the infix expression:\n");
+	scanf("%s",&infix);
+	
+	for(i=0;i<=strlen(infix);i++)
+	{
+		if((infix[i]>='0' && infix[i]<='9') || (infix[i]>='a' && infix[i]<='z') || (infix[i]>='A' && infix[i]<='Z'))
+		{
+			a[b++]=infix[i];
+		}
+		else if(infix[i]=='+' || infix[i]=='-' || infix[i]=='*' || infix[i]=='/' || infix[i]=='$')
+		{
+			w=symbolValue(infix[i]);
+		//	printf("infix char=%c and w=%d",infix[i],w);
+			if(s.top==-1)
+			{
+				push(&s,infix[i]);
+			//	printf("the pushed item is %c",infix[i]);
+			}
+			else if(!(s.top==-1))
+			{
+				y=pop(&s);
+			//	printf("the popped item is:%c\n",y);
+				push(&s,y);
+				if(w>(k=symbolValue(y)))
+				{
+					push(&s,infix[i]);
+				//	printf("the pushed item is:%c and k=%d\n",infix[i],k);
+				}
+				else if(w<=(k=symbolValue(y)))
+				{
+					a[b++]=pop(&s);
+				//	printf("the popped item is: %c\n",a[b++]);
+					push(&s,infix[i]);
+				//	printf("the pushed item is: %c and k=%d",infix[i],k);
+				}
+			}	
+		}
+	}
+	while(!(s.top==-1))
+	{
+		a[b++]=pop(&s);
+	}
+	printf("The postfix expression of given infix expression is: %s",a);
+}
+
+int symbolValue(char x)
+{
+	if(x=='+' || x=='-')
+	{
+		return 1;
+	}
+	else if(x=='*' || x=='/' || x=='%')
+	{
+		return 2;
+	}
+	else if(x=='$')
+	{
+		return 3;
+	}
+}
+
+char push(stack *s, char x)
+{
+	s->items[++(s->top)]=x;
+}
+
+int pop(stack *s)
+{
+	int x;
+	x=s->items[(s->top)--];
+	return x;
+}
+
